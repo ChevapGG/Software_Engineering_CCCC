ff4dc95c67e5f81fca54caeee3009dce6c2271ef
ChevapGG <49946398+ChevapGG@users.noreply.github.com>
2020-05-14 13:01:19 +0200

    added first dlg options

---
+++ Projektordner Java/Produkt/src/main/CCCC_Dlg.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.*;
+
+public class CCCC_Dlg {
+    private JTextArea esGibt23UnterartenTextArea;
+    private JTextField textField1;
+    private JButton codeEinloesenButton;
+    private JButton fuetternButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
