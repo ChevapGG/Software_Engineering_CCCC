cb95de0e8003fdb14acc34d5800263fbef0a4197
ChevapGG <49946398+ChevapGG@users.noreply.github.com>
2020-05-17 19:18:28 +0200

    show GUI

--- Projektordner Java/Produkt/src/main/CCCC_Dlg.java
+++ Projektordner Java/Produkt/src/main/CCCC_Dlg.java
@@ -7,8 +7,24 @@ public class CCCC_Dlg {
     private JTextField textField1;
     private JButton codeEinloesenButton;
     private JButton fuetternButton;
+    private JPanel panelMain;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World !!!");
+
+        JFrame frame = new JFrame("Cherry Chipmunks Cereal Choice");
+        frame.setContentPane(new CCCC_Dlg().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
 }
