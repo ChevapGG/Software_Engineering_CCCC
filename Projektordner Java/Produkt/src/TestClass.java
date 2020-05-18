ec023471cbf3ad93f1db6b58bf2d8669587c91c6
ChevapGG <49946398+ChevapGG@users.noreply.github.com>
2020-05-18 23:16:00 +0200

    preparations for testng and gradle

--- Projektordner Java/Produkt/src/TestClass.java
+++ Projektordner Java/Produkt/src/TestClass.java
@@ -1,8 +1,25 @@
-public class TestClass
-{
+import org.testng.annotations.Test;
 
-        public static void main(String[] args)
-        {
-            System.out.println("Hello, World !!!");
-        }
+public class TestClass {
+
+    @Test(priority = 1)
+    void setUp()
+    {
+        // code that will be invoked when this test is instantiated
+        //open window
+        System.out.println("Setup");
+    }
+
+@Test(priority = 2)
+    void test1()
+    {
+        System.out.println("test1");
+    }
+
+    @Test(priority = 3)
+    void test2()
+    {
+        System.out.println("test2");
+    }
+
 }
